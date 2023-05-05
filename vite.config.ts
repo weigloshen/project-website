e0344b4095bde64583fcc1675a11cf903f4ea71c
weigloshen <weishusheng100@gmail.com>
2023-05-05 21:39:36 +0800

    修改vite配置静态网页

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/project-website/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,7 +13,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: "@import '@/assets/root.scss';",  // css全局变量颜色
+        additionalData: "@import '@/assets/root.scss';", // css全局变量颜色
       },
     },
   },
